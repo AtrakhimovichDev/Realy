645353a4ff23b95e95daae453152e7ecfe9f29be
Elena Pospelova <pospelova_elena_al@mail.ru>
2026-01-27 18:31:33 +0500

    Initial Commit

---
+++ Realy/RealyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealyApp.swift
+//  Realy
+//
+//  Created by Elena Pospelova on 27.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RealyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
